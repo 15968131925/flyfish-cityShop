00ffc3cc2268cb2a2c7cf54dec85f6d5e6e7c21a
cql <80401270@qq.com>
2022-10-16 00:54:42 +0800

    文件提交

---
+++ src/main/java/com/cql/flyfishcityshop/FlyfishCityshopApplication.java
@@ -0,0 +1,13 @@
+package com.cql.flyfishcityshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyfishCityshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyfishCityshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cql/flyfishcityshop/FlyfishCityshopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cql.flyfishcityshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyfishCityshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        System.out.println("helloWorld");
+
+    }
+
+}
