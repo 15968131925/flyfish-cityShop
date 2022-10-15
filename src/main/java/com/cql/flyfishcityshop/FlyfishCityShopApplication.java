cd3eedc8680ebf018e6b2b66145018170a5a3631
cql <80401270@qq.com>
2022-10-16 01:01:00 +0800

    修改项目名，主启动类名

--- src/main/java/com/cql/flyfishcityshop/FlyfishCityShopApplication.java
+++ src/main/java/com/cql/flyfishcityshop/FlyfishCityShopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlyfishCityshopApplication {
+public class FlyfishCityShopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FlyfishCityshopApplication.class, args);
+        SpringApplication.run(FlyfishCityShopApplication.class, args);
     }
 
 }
